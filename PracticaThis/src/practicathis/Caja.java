02affc9515f85910251d7ffc41a7071657f6e9bc
Creator125 <obedagualimpia1@gmail.com>
2023-09-19 21:16:25 -0500

    Nuevas practicas sobre 'this'

---
+++ PracticaThis/src/practicathis/Caja.java
@@ -0,0 +1,16 @@
+
+package practicathis;
+
+public class Caja {
+    double ancho, largo, alto, volumenCaja;
+
+    public Caja() {
+    }
+
+    public Caja(double ancho, double largo, double alto, double volumenCaja) {
+        this.ancho = ancho;
+        this.largo = largo;
+        this.alto = alto;
+        this.volumenCaja = volumenCaja;
+    }
+}
---
+++ PracticaThis/src/practicathis/PracticaThis.java
@@ -0,0 +1,9 @@
+package practicathis;
+
+public class PracticaThis {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
