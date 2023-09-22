7edc0f3436a6db856f8439c63b703efbff3af91d
Creator125 <obedagualimpia1@gmail.com>
2023-09-21 19:28:58 -0500

    CentroAdoptivo/: Instanciando las clases

--- CentroAdoptivo/src/centroadoptivo/CentroAdoptivo.java
+++ CentroAdoptivo/src/centroadoptivo/CentroAdoptivo.java
@@ -10,6 +10,24 @@ public class CentroAdoptivo {
         
         System.out.println("Nombre del perro1: "+perro1.getNombre());
         System.out.println("Nombre del perro2: "+perro2.getNombre());
+        perro2.mostrarInfo();
+        
+        //Creando el gato
+        Gato gato1 = new Gato("Manqui",9, 10, 02);
+        gato1.mostrarInfo();
+        
+        //Creando el loro
+        Loro loro1 = new Loro("Milkin",5, 5, 03);
+        loro1.mostrarInfo();
+        
+        //Creando el raton
+        Raton mirata = new Raton("Sarilla",7, 3, 04);
+        mirata.mostrarInfo();
+        
+        //Creando el conejo
+        Conejo conejo1 = new Conejo("Pelusa",14, 10, 05);
+        conejo1.mostrarInfo();
+        
     }
     
 }
